c71b4adaea2e50a9487a791f18df44d1f69ba1fe
PedramMarandi <pedram.marandi@gmail.com>
2026-03-25 13:42:14 +1100

    windows UI fix

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"os"
 )
 
 func main() {
+	log.SetOutput(io.Discard)
+
 	ui := newUI()
 	if err := ui.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
